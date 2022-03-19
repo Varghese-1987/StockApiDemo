cc91a9d1704d957ddcb3dadf1959650fb52e9411
Varghese Cherian <vcdenied@gmail.com>
2022-03-19 23:34:23 +0800

    added some images for the readme file

--- StockApi.Domain/Entities/StockEnities/StockPrice.cs
+++ StockApi.Domain/Entities/StockEnities/StockPrice.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StockApi.Domain.Entities.StockEnities
+﻿namespace StockApi.Domain.Entities.StockEnities
 {
     public class StockPrice
     {
